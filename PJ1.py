61c3c3dfe219f9ea776d3f2c79ece0c31ee4b6fa
FURYFULL <higor-ormonde@hotmail.com>
2024-03-04 10:18:06 -0400

    att

--- PJ1.py
+++ PJ1.py
@@ -2,14 +2,16 @@ import random
 numb = {"1": 2, "2": 5, "5": 3, '4': 5}
 lst = []
 for i in numb:
-    # print(numb[i])
     lst.extend(int(i) for x in range(numb[i]))
 print(lst)
-jogo = 3
-while True:
-    rnd = random.sample(lst, jogo)
-    # set gets unique elements
-    if len(rnd) == len(set(rnd)):
-        break
-print(rnd)
+# usar o Theme Draculo VSCode
+quant_de_numeros = 3
+jogos = 3
 
+for jogo in range(1, jogos+1):
+    while True:
+        rnd = random.sample(lst, quant_de_numeros)
+        # set gets unique elements
+        if len(rnd) == len(set(rnd)):
+            break
+    print(rnd)
