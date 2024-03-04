b104c0fadc01dbd832d87b16fdffa9dee67201de
FURYFULL <higor-ormonde@hotmail.com>
2024-03-04 09:55:29 -0400

    att

--- PJ1.py
+++ PJ1.py
@@ -1,2 +1,15 @@
-print("testes de viluzalizaçãoaosod")
-print("comideee")
+import random
+numb = {"1": 2, "2": 5, "5": 3, '4': 5}
+lst = []
+for i in numb:
+    # print(numb[i])
+    lst.extend(int(i) for x in range(numb[i]))
+print(lst)
+jogo = 3
+while True:
+    rnd = random.sample(lst, jogo)
+    # set gets unique elements
+    if len(rnd) == len(set(rnd)):
+        break
+print(rnd)
+
