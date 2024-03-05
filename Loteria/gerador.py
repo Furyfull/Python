bb43ffe3fbd18b5de65d113cbee6a8fd4161b488
FURYFULL <higor-ormonde@hotmail.com>
2024-03-05 08:54:51 -0400

    att

---
+++ Loteria/gerador.py
@@ -0,0 +1,29 @@
+import random
+import json
+
+
+def cartela(numb, jog, numeros):
+    assert isinstance(numb, dict)
+    lst = []
+    resposta = []
+    for i in numb:
+        lst.extend(int(i) for x in range(numb[i]))
+    # print(lst)
+    quant_de_numeros = numeros
+    jogos = jog
+
+    for jogo in range(1, jogos + 1):
+        while True:
+            rnd = random.sample(lst, quant_de_numeros)
+            # set gets unique elements
+            if len(rnd) == len(set(rnd)):
+                break
+
+        rnd.sort()
+        resposta.append(rnd)
+    return resposta
+
+
+database = json.load(open('numeros_da_lotofacil.json', 'r'))
+gg = cartela(database, 3, 15)
+print(*gg, sep='\n')
