83be948216430b2113abd2bb0aff6dd557b33d4b
Furyfull <higor-ormonde@hotmail.com>
2024-03-05 06:49:36 -0400

    att

--- Loteria/PJ1.py
+++ Loteria/PJ1.py
@@ -1,17 +1,30 @@
 import random
-numb = {"1": 2, "2": 5, "5": 3, '4': 5}
-lst = []
-for i in numb:
-    lst.extend(int(i) for x in range(numb[i]))
-print(lst)
-# usar o Theme Draculo VSCode
-quant_de_numeros = 3
-jogos = 3
+import json
 
-for jogo in range(1, jogos+1):
-    while True:
-        rnd = random.sample(lst, quant_de_numeros)
-        # set gets unique elements
-        if len(rnd) == len(set(rnd)):
-            break
-    print(rnd)
+
+def cartela(numb,jog,numeros):
+    assert isinstance(numb, dict)
+    lst = []
+    resposta = []
+    for i in numb:
+        lst.extend(int(i) for x in range(numb[i]))
+    print(lst)
+    # usar o Theme Draculo VSCode
+    quant_de_numeros = numeros
+    jogos = jog
+
+    for jogo in range(1, jogos + 1):
+        while True:
+            rnd = random.sample(lst, quant_de_numeros)
+            # set gets unique elements
+            if len(rnd) == len(set(rnd)):
+                break
+
+        rnd.sort()
+        resposta += [rnd]
+        print(resposta)
+
+
+numb = json.load(open('numeros_da_lotofacil.json', 'r'))
+numb = "4rr"
+cartela(numb, 3, 15)
